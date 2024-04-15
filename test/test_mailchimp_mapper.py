c941f0070e983c85f8124cd890a4b535058a62a5
Bárbara Sousa <barbaravieirasousa@gmail.com>
2024-04-16 00:11:25 +0100

    feat: Add mapper and sync tests

---
+++ test/test_mailchimp_mapper.py
@@ -0,0 +1,62 @@
+import unittest
+from mailchimp.mailchimp_mapper import parse_contact, parse_contact_list
+from models import Contact
+
+
+class TestParsingMethods(unittest.TestCase):
+
+    def test_parse_contact(self):
+        mailchimp_contact = {
+            "contact_id": "123",
+            "email_address": "thor@gmail.com",
+            "status": "subscribed",
+            "merge_fields": {
+                "FNAME": "Thor",
+                "LNAME": "Odinson"
+            }
+        }
+
+        expected_contact = Contact(id="123", firstname="Thor", lastname="Odinson", email="thor@gmail.com",
+                                   status="subscribed")
+
+        self.assertEqual(parse_contact(mailchimp_contact), expected_contact)
+
+    def test_parse_contact_missing_fields(self):
+        mailchimp_contact = {
+            "contact_id": "456",
+            "email_address": "jane@gmail.com",
+            "status": "unsubscribed",
+        }
+        expected_contact = Contact(id="456", firstname="", lastname="", email="jane@gmail.com",
+                                   status="unsubscribed")
+        self.assertEqual(parse_contact(mailchimp_contact), expected_contact)
+
+    def test_parse_contact_list(self):
+        mailchimp_contact_list = {
+            "members": [
+                {
+                    "contact_id": "123",
+                    "email_address": "ironman@gmail.com",
+                    "status": "subscribed",
+                    "merge_fields": {
+                        "FNAME": "Iron",
+                        "LNAME": "Man"
+                    }
+                },
+                {
+                    "contact_id": "456",
+                    "email_address": "peterparker@gmail.com",
+                    "status": "unsubscribed",
+                }
+            ]
+        }
+        expected_contacts = [
+            Contact(id="123", firstname="Iron", lastname="Man", email="ironman@gmail.com", status="subscribed"),
+            Contact(id="456", firstname="", lastname="", email="peterparker@gmail.com", status="unsubscribed")
+        ]
+        self.assertEqual(parse_contact_list(mailchimp_contact_list), expected_contacts)
+
+    def test_parse_empty_list(self):
+        mailchimp_contact_list = {"members": []}
+        expected_contacts = []
+        self.assertEqual(parse_contact_list(mailchimp_contact_list), expected_contacts)
---
+++ test/test_sync_util.py
@@ -0,0 +1,39 @@
+import json
+import unittest
+from datetime import datetime
+import os
+
+from sync_util import get_last_sync_time, update_last_sync_time
+
+
+class TestLastSyncTime(unittest.TestCase):
+    def setUp(self):
+        with open('last_sync_time.json', 'w') as file:
+            file.write('{"last_sync": "2024-04-01T12:00:00"}')
+
+    def test_get_last_sync_time_existing_file(self):
+        last_sync = get_last_sync_time()
+        self.assertEqual(last_sync, datetime(2024, 4, 1, 12, 0, 0))
+
+    def test_get_last_sync_time_file_not_exist(self):
+        if os.path.exists('last_sync_time.json'):
+            os.remove('last_sync_time.json')
+
+        result = get_last_sync_time()
+
+        self.assertIsNone(result)
+
+    def test_update_last_sync_time(self):
+        # Get the current datetime
+        current_time = datetime.now().isoformat()
+
+        # Call the function to update the last sync time
+        update_last_sync_time()
+
+        # Read the content of the last_sync_time.json file
+        with open('last_sync_time.json', 'r') as file:
+            data = json.load(file)
+
+        # Check if the updated time is after the previous time
+        updated_time = datetime.fromisoformat(data['last_sync'])
+        self.assertGreaterEqual(updated_time, datetime.fromisoformat(current_time))
